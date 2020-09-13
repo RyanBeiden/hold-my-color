864326fe084f58062a724d0623e20d27e2e99007
Ryan Beiden <rdbeiden@gmail.com>
2020-09-12 19:51:57 -0500

    unauthorized home page layout / responsive

--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -5,6 +5,7 @@ import 'firebase/auth';
 import {
   Button,
   Container,
+  Divider,
 } from '@material-ui/core';
 
 import './Auth.scss';
@@ -27,14 +28,19 @@ class Auth extends React.Component {
   render() {
     return (
       <div className="Auth">
-        <Container>
+        <h2>Build your color palette with ease.</h2>
+        <h3>More information here about the features of this web app</h3>
+        <Container className="Auth__container">
+          <h4>Sign in to build a palette</h4>
+          <Divider />
           <div className="Auth__button">
-            <Button variant="outlined" onClick={this.googleSignIn}>Google Sign In</Button>
+            <Button variant="outlined" onClick={this.googleSignIn}><i className="fab fa-google"></i> Sign in with Google</Button>
           </div>
           <div className="Auth__button">
-            <Button variant="outlined" onClick={this.githubSignIn}>Github Sign In</Button>
+            <Button variant="outlined" onClick={this.githubSignIn}><i className="fab fa-github"></i> Sign in with Github</Button>
           </div>
         </Container>
+        <footer className="Auth__created-by">Created by Ryan Beiden</footer>
       </div>
     );
   }
