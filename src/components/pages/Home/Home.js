19783845dc27b56d2af564468d4c20aaf63f5581
Ryan Beiden <rdbeiden@gmail.com>
2020-09-14 12:10:46 -0500

    print palettes by uid on home page / responsive

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -2,15 +2,43 @@ import React from 'react';
 
 import Button from '@material-ui/core/Button';
 
+import authData from '../../../helpers/data/authData';
+import paletteData from '../../../helpers/data/paletteData';
+import PaletteCard from '../../shared/PaletteCard/PaletteCard';
+
 import './Home.scss';
 
 class Home extends React.Component {
+  state = {
+    palettes: [],
+  }
+
+  componentDidMount() {
+    this.getPalettes();
+  }
+
+  getPalettes = () => {
+    paletteData.getPalettesByUid(authData.getUid())
+      .then((palettes) => this.setState({ palettes }))
+      .catch((err) => console.error('Getting the palettes by uid did not work -> ', err));
+  }
+
   render() {
+    const { palettes } = this.state;
+
+    const paletteCards = palettes.map((palette) => <PaletteCard
+      key={palette.id}
+      palette={palette}
+    />);
+
     return (
       <div className="Home">
         <div className="Home__title">
           <h2>My Palettes</h2>
           <Button className="Home__new-button" variant="outlined"><i className="fas fa-plus"></i></Button>
+        </div>
+        <div className="Home__palette-container">
+          {paletteCards}
         </div>
       </div>
     );
